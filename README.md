d7268b37f3d4d630aa528524ff8462b0cd3c9a72
qetzing <general@qetz.de>
2023-11-24 18:15:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This application is the backend-side for the management of my last school year.
+Its main purpose is to review student's and teacher's quotes.
+Thus, an authentication and authorization layer as well as other core functionalities
+have to be implemented.
---
+++ backend/src/main/java/derszteler/abimanagement/Application.java
@@ -0,0 +1,11 @@
+package derszteler.abimanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(String[] options) {
+    SpringApplication.run(Application.class, options);
+  }
+}
