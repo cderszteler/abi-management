3a5d8137dad91af41609d09ace1d4ee11e5883df
qetzing <general@qetz.de>
2023-11-30 21:11:03 +0100

    Localize not-found.tsx

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -4,6 +4,7 @@ import {Container} from '@/components/Container'
 import {FadeIn} from '@/components/FadeIn'
 
 export default function NotFound() {
+  // TODO: Or return to dashboard
   return (
     <Container className="flex h-full items-center pt-24 sm:pt-32 lg:pt-40">
       <FadeIn className="flex max-w-xl flex-col items-center text-center">
@@ -11,16 +12,16 @@ export default function NotFound() {
           404
         </p>
         <h1 className="mt-4 font-display text-2xl font-semibold text-neutral-950">
-          Page not found
+          Seite nichte gefunden
         </h1>
         <p className="mt-2 text-sm text-neutral-600">
-          Sorry, we couldn’t find the page you’re looking for.
+          Entschuldigung, wir konnten die gesuchte Seite nicht finden.
         </p>
         <Link
           href="/"
           className="mt-4 text-sm font-semibold text-neutral-950 transition hover:text-neutral-700"
         >
-          Go to the home page
+          Zurück zur Startseite
         </Link>
       </FadeIn>
     </Container>
