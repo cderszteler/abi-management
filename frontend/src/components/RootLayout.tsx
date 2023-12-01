afdb5ffd4626cfe65046e40b884a1cbe95a9ccb3
qetzing <general@qetz.de>
2023-12-01 21:57:22 +0100

    Fix navigation

--- frontend/src/components/RootLayout.tsx
+++ frontend/src/components/RootLayout.tsx
@@ -123,18 +123,27 @@ function NavigationRow({ children }: { children: React.ReactNode }) {
 
 function NavigationItem({
   href,
+  disabled,
   children,
 }: {
   href: string
+  disabled?: boolean
   children: React.ReactNode
 }) {
   return (
     <Link
       href={href}
-      className="group relative isolate -mx-6 bg-neutral-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16"
+      className={clsx(
+        "group relative isolate -mx-6 bg-neutral-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16",
+        disabled ? "cursor-default pointer-events-none" : ""
+      )}
     >
       {children}
-      <span className="absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
+      <span className={clsx(
+        "absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 group-odd:right-0 group-even:left-0 transition",
+        disabled ? "" : "group-hover:opacity-100"
+        )}
+      />
     </Link>
   )
 }
@@ -144,12 +153,8 @@ function Navigation() {
   return (
     <nav className="mt-px font-display text-5xl font-medium tracking-tight text-white">
       <NavigationRow>
-        <NavigationItem href="/work">Our Work</NavigationItem>
-        <NavigationItem href="/about">About Us</NavigationItem>
-      </NavigationRow>
-      <NavigationRow>
-        <NavigationItem href="/process">Our Process</NavigationItem>
-        <NavigationItem href="/blog">Blog</NavigationItem>
+        <NavigationItem href="/dashboard/quotes">Zitate</NavigationItem>
+        <NavigationItem href="/" disabled>Coming soon...</NavigationItem>
       </NavigationRow>
     </nav>
   )
