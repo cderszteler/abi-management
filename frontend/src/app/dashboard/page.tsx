29b7fe84fac30c16ad50d9f6e962bdeebc42ffa5
qetzing <general@qetz.de>
2023-12-13 10:20:11 +0100

    Add metadata to dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,10 @@
+import {Metadata} from "next"
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+  description: 'Willkommen im Dashboard!',
+}
+
 export default function Example() {
   return (
     <h1 className="text-neutral-950">Welcome!</h1>
