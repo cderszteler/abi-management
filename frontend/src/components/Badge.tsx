270894c147af8cdbd4aa50cd18327a7457b1f786
qetzing <general@qetz.de>
2023-12-16 23:23:47 +0100

    Fix Badge styling

--- frontend/src/components/Badge.tsx
+++ frontend/src/components/Badge.tsx
@@ -3,10 +3,10 @@ import clsx from "clsx"
 export type Color = 'red' | 'yellow' | 'green' | 'gray'
 
 const classNames = {
-  'red': "inline-flex items-center rounded-full bg-red-50 px-2 py-1 text-xs font-medium text-red-600 ring-1 ring-inset ring-red-500/10",
-  'yellow': "inline-flex items-center rounded-full bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-600 ring-1 ring-inset ring-yellow-500/10",
-  'green': "inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-600 ring-1 ring-inset ring-green-500/10",
-  'gray': "inline-flex items-center rounded-full bg-gray-50 px-2 py-1 text-xs font-medium text-gr-600 ring-1 ring-inset ring-gray-500/10"
+  'red': "bg-red-50 text-red-600 ring-red-500/10",
+  'yellow': "bg-yellow-50 text-yellow-600 ring-yellow-500/10",
+  'green': "bg-green-50 text-green-600 ring-green-500/10",
+  'gray': "bg-gray-50 text-gr-600 ring-gray-500/10"
 }
 
 export function PillWithBorder({color, children}: {
@@ -14,7 +14,13 @@ export function PillWithBorder({color, children}: {
   children: React.ReactNode
 }) {
   return (
-    <span className={clsx(classNames[color], "cursor-pointer")} data-tooltip-target="tooltip-animation">
+    <span
+      className={clsx(
+        "inline-flex items-center rounded-full px-2 py-1 text-xs font-medium text-center ring-1 ring-inset cursor-pointer",
+        classNames[color]
+      )}
+      data-tooltip-target="tooltip-animation"
+    >
       {children}
     </span>
   )
