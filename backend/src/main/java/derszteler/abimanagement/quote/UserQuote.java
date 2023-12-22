6e85dd7c6ab046d22a6f254844131ecdfb002721
qetzing <general@qetz.de>
2023-12-22 16:12:06 +0100

    Add Jackson deserializer for UserQuote

--- backend/src/main/java/derszteler/abimanagement/quote/UserQuote.java
+++ backend/src/main/java/derszteler/abimanagement/quote/UserQuote.java
@@ -2,12 +2,20 @@ package derszteler.abimanagement.quote;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import derszteler.abimanagement.quote.review.QuoteReview;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 
+import java.io.IOException;
+
 import static derszteler.abimanagement.quote.UserQuote.Status.*;
 
+@JsonDeserialize(using = UserQuote.UserQuoteDeserializer.class)
 public record UserQuote(
   @JsonIgnore
   @Schema(hidden = true)
@@ -19,13 +27,13 @@ public record UserQuote(
 ) {
   @Schema(description = "The id of the quote", example = "1")
   @JsonProperty
-  int id() {
+  public int id() {
     return quote.id();
   }
 
   @Schema(description = "The content of the quote", example = "I am a cyborg.")
   @JsonProperty
-  String content() {
+  public String content() {
     return quote().content();
   }
 
@@ -35,7 +43,7 @@ public record UserQuote(
     nullable = true
   )
   @JsonProperty
-  String context() {
+  public String context() {
     return quote().context();
   }
 
@@ -68,5 +76,33 @@ public record UserQuote(
     Pending,
     Rejected,
     NotAllowed
+  }
+
+  static final class UserQuoteDeserializer extends StdDeserializer<UserQuote> {
+    @SuppressWarnings("unused")
+    public UserQuoteDeserializer() {
+      this(null);
+    }
+
+    public UserQuoteDeserializer(Class<?> valueClass) {
+      super(valueClass);
+    }
+
+    @Override
+    public UserQuote deserialize(
+      JsonParser parser,
+      DeserializationContext context
+    ) throws IOException {
+      var node = (ObjectNode) parser.getCodec().readTree(parser);
+
+      return new UserQuote(
+        Quote.builder()
+          .id(node.get("id").intValue())
+          .content(node.get("content").textValue())
+          .context(node.get("context").textValue())
+          .build(),
+        null
+      );
+    }
   }
 }
