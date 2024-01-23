002597b7ecfbe96538db673c38ec63c6b8456ae7
qetzing <general@qetz.de>
2024-01-23 14:55:53 +0100

    Improve PillWithBorder's styling

--- frontend/src/components/Badge.tsx
+++ frontend/src/components/Badge.tsx
@@ -3,10 +3,10 @@ import clsx from "clsx"
 export type Color = 'red' | 'yellow' | 'green' | 'gray'
 
 const classNames = {
-  'red': "bg-red-50 text-red-600 ring-red-500/10",
-  'yellow': "bg-yellow-50 text-yellow-600 ring-yellow-500/10",
-  'green': "bg-green-50 text-green-600 ring-green-500/10",
-  'gray': "bg-gray-50 text-gray-600 ring-gray-500/10"
+  'red': "bg-red-50 text-red-600 ring-red-500/50",
+  'yellow': "bg-yellow-50 text-yellow-600 ring-yellow-500/50",
+  'green': "bg-green-50 text-green-600 ring-green-500/50",
+  'gray': "bg-gray-50 text-gray-600 ring-gray-500/50"
 }
 
 export function PillWithBorder({color, className, children}: {
